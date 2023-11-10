542562321e22c1b01e25aab417957eab6d649d04
sparkerhoney <lhe339@gmail.com>
2023-11-10 11:26:01 +0900

    Docker

--- SpeechT5_Serving/app.py
+++ SpeechT5_Serving/app.py
@@ -1,17 +1,15 @@
 from flask import Flask, request, jsonify
 import torch
 from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
-from transformers import pipeline
 import soundfile as sf
 from datasets import load_dataset
-
-processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
 
 app = Flask(__name__)
 
 # Initialize SpeechT5 synthesizer
-synthesizer = pipeline("text-to-speech", model="microsoft/speecht5_tts")
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
 
 @app.route('/tts', methods=['POST'])
 def tts():
@@ -22,11 +20,14 @@ def tts():
     embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
     speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
     
+    # Preprocess input text
+    input_ids = processor(text=input_text, return_tensors="pt").input_ids
+    
     # Generate speech
-    speech = synthesizer(input_text, forward_params={"speaker_embeddings": speaker_embedding})
+    speech = model.generate_speech(input_ids, speaker_embedding, vocoder=vocoder)
     
     # Save the speech to a file
-    sf.write("speech.wav", speech["audio"], samplerate=speech["sampling_rate"])
+    sf.write("speech.wav", speech.numpy(), samplerate=16000)
     
     # Respond with success message and file path
     response = {
